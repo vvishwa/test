76c951a83dd393ae3de048430d7042af8263e3c1
bitbucket_admin <vvishwakarma@jefferies.com>
2017-02-27 17:29:57 -0500

    no message

--- src/core/algo/string/DynProVerticalSticks.java
+++ src/core/algo/string/DynProVerticalSticks.java
@@ -8,18 +8,29 @@ public class DynProVerticalSticks {
     private static int sum(int arr[]) {
         int i = arr.length - 1;
         while (i > 0) {
-            if (arr[arr.length-1] >= arr[i])
+            if (arr[arr.length - 1] <= arr[i-1])
                 break;
+            i--;
         }
         if (arr.length == 1)
             return 1;
         else
-            return arr.length - 1 - i + sum(Arrays.copyOf(arr, arr.length - 1));
+            return arr.length - i + sum(Arrays.copyOf(arr, arr.length - 1));
     }
     
-    private static void avgArr(int[] arr) {
-        int s = sum(arr);
-        System.out.println(s/arr.length); 
+    private static void avgArr(int[] arr, int left, int right) {
+        //double s = sum(arr);
+        //System.out.println(s);
+        //arr[0] and arr[1] until end
+    	if (right > left)
+    		return;
+        if (right > arr.length -1) {
+        	int tmp = arr[1];
+        	arr[1] = arr[0];
+        	arr[0] = tmp;
+        } else {
+        	int[] ar1 = Arrays.copyOfRange(arr, 1, arr.length-1);
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -31,7 +42,7 @@ public class DynProVerticalSticks {
             int[] arr = new int[noEle];
             for (int i = 0; i < noEle; i++)
                 arr[i] = in.nextInt();
-            avgArr(arr);
+            //avgArr(arr);
         }
     }
 }
